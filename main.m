e7ac0feffd35d1ea9520603ed13112557509e616
Youngwoo Kim <yw.sean.kim@gmail.com>
2023-12-16 17:08:56 +0900

    feat/saving and loading mat file

--- main.m
+++ main.m
@@ -1,8 +1,21 @@
-function main(imgpath)
+function main(imgpath, nimpath)
+    arguments
+        % Path to NiFTI image file
+        imgpath string
+
+        % Path to save processed .mat file (must end in `.mat`)
+        nimpath string
+    end
+
+    start_time = datetime('now', 'Format', 'yyyy-MM-dd hh:mm:ss');
+    fprintf("HINEC START: %s\n", string(start_time));
+
     nim = nim_read(imgpath);
     nim = nim_dt_spd(nim);
     nim = nim_eig(nim);
     nim = nim_fa(nim);
-    
-    save nim_processed.mat nim;
+    nim_save(nim, nimpath);
+
+    end_time = datetime('now', 'Format', 'yyyy-MM-dd hh:mm:ss');
+    fprintf("HINEC END: %s\n", string(end_time));
 end
