64c9b2d7ece342fe56922987c6c294f71d8047e0
TooSalty <12saltyh@gmail.com>
2024-07-07 09:49:22 +0800

    add path at main

--- main.m
+++ main.m
@@ -1,21 +1,27 @@
 function main(imgpath, nimpath)
-    arguments
-        % Path to NiFTI image file
-        imgpath string
+arguments
+    % Path to NiFTI image file
+    imgpath string
+    
+    % Path to save processed .mat file (must end in `.mat`)
+    nimpath string
+end
 
-        % Path to save processed .mat file (must end in `.mat`)
-        nimpath string
-    end
+% include folders to path
+addpath('nim_utils');
+addpath('utils');
+addpath('bfgs');
 
-    start_time = datetime('now', 'Format', 'yyyy-MM-dd hh:mm:ss');
-    fprintf("HINEC START: %s\n", string(start_time));
 
-    nim = nim_read(imgpath);
-    nim = nim_dt_spd(nim);
-    %nim = nim_eig(nim);
-    nim = nim_fa(nim);
-    nim_save(nim, nimpath);
+start_time = datetime('now', 'Format', 'yyyy-MM-dd hh:mm:ss');
+fprintf("HINEC START: %s\n", string(start_time));
 
-    end_time = datetime('now', 'Format', 'yyyy-MM-dd hh:mm:ss');
-    fprintf("HINEC END: %s\n", string(end_time));
+nim = nim_read(imgpath);
+nim = nim_dt_spd(nim);
+%nim = nim_eig(nim);
+nim = nim_fa(nim);
+nim_save(nim, nimpath);
+
+end_time = datetime('now', 'Format', 'yyyy-MM-dd hh:mm:ss');
+fprintf("HINEC END: %s\n", string(end_time));
 end
